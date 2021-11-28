68a96d3a19ee15e0a2e5bfcecd8a3e3598127d26
Patrick Hüntelmann <patrick.huentelmann@gmail.com>
2021-11-28 19:08:55 +0100

    fix Schedule serialization

--- FHGuide.Shared/Models/Schedule.cs
+++ FHGuide.Shared/Models/Schedule.cs
@@ -1,11 +1,14 @@
-﻿namespace FHGuide.Shared.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace FHGuide.Shared.Models;
 
 public partial class Schedule
 {
     public int ScheduleId { get; set; }
-    public DateOnly? ExpireDate { get; set; }
-    public DateOnly CreateDate { get; set; }
+    public DateTime? ExpireDate { get; set; }
+    public DateTime CreateDate { get; set; }
     public int AccountId { get; set; }
 
+	[JsonIgnore]
     public virtual Account Account { get; set; } = null!;
 }
