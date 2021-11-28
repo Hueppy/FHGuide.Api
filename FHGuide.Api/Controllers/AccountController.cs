4bc871b0cfd123d08e1fc0c49a4d4eb8e920c21f
Patrick Hüntelmann <patrick.huentelmann@gmail.com>
2021-11-28 16:01:53 +0100

    implement account controller

--- FHGuide.Api/Controllers/AccountController.cs
+++ FHGuide.Api/Controllers/AccountController.cs
@@ -21,54 +21,79 @@ public class AccountController : ControllerBase
 	[HttpGet]
 	public IEnumerable<Account> Get()
 	{
-		// TODO: Implement this
-		return Enumerable.Empty<Account>();
+		return this.DbContext.Accounts;
 	}
 
 	/// <summary>
 	/// Get account with specified id
 	/// </summary>
+	/// <response code="404">Account could not be found</response>
 	[HttpGet("{id}")]
-	public Account? Get(int id)
+	public async Task<ActionResult<Account>> Get(int id)
 	{
-		// TODO: Implement this
-		return null;
+		var item = await this.DbContext.Accounts.FindAsync(id);
+
+		if (item == null)
+		{
+			return NotFound();
+		}
+
+		return item;
 	}
 
 	/// <summary>
 	/// Get schedule for account
 	/// </summary>
+	/// <response code="404">Account could not be found</response>
 	[HttpGet("{id}/schedule")]
-	public IEnumerable<Schedule> GetSchedule(int id)
+	public async Task<ActionResult<IEnumerable<Schedule>>> GetSchedule(int id)
 	{
-		// TODO: Implement this
-		return Enumerable.Empty<Schedule>();
+		var item = await this.DbContext.Accounts.FindAsync(id);
+
+		if (item == null)
+		{
+			return NotFound();
+		}
+
+		return Ok(this.DbContext.Schedules.Where((x) => x.AccountId == id));
 	}
 
 	/// <summary>
 	/// Create new account
 	/// </summary>
 	[HttpPost]
-	public void Post(Account account)
+	public async Task Post(Account account)
 	{
-		// TODO: Implement this
+		await this.DbContext.Accounts.AddAsync(account);
+		await this.DbContext.SaveChangesAsync();
 	}
 
 	/// <summary>
 	/// Update account with specified id
 	/// </summary>
 	[HttpPatch("{id}")]
-	public void Patch(int id, Account account)
+	public async Task Patch(int id, Account account)
 	{
-		// TODO: Implement this
+		account.AccountId = id;
+		this.DbContext.Accounts.Update(account);
+		await this.DbContext.SaveChangesAsync();
 	}
 
 	/// <summary>
 	/// Deletes account with specified id
 	/// </summary>
 	[HttpDelete("{id}")]
-	public void Delete(int id)
+	public async Task<ActionResult> Delete(int id)
 	{
-		// TODO: Implement this
+		var item = await this.DbContext.Accounts.FindAsync(id);
+
+		if (item == null)
+		{
+			return NotFound();
+		}
+
+		this.DbContext.Accounts.Remove(item);
+        await this.DbContext.SaveChangesAsync();
+		return Ok();
 	}
 }
